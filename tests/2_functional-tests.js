f32480330dd1a1aaba9949206def8031742fa00b
linalumburovska <linalumburovska20@gmail.com>
2019-03-26 16:46:35 +0100

    Functional tests added

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,45 +15,57 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
-  suite('API ROUTING FOR /api/threads/:board', function() {
+  suite('API ROUTING FOR /api/threads', function() {
     
     suite('POST', function() {
-      
-    });
+      test('Test POST /api/threads', function(done) {
+        chai.request(server)
+          .post('/api/threads')
+          .send({"text": "text",
+          "delete_password":"del"})
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.property(res.body, '_id');
+            assert.property(res.body, 'text');
+            assert.property(res.body, 'replies');
+            assert.property(res.body, 'delete_password');
+            assert.equal(res.body.text, 'text');
+            assert.equal(res.body.delete_password, 'del');
+            assert.isArray(res.body.replies);
+            assert.lengthOf(res.body.replies, 0);
+            done()
+          });
+        });
+      });
     
     suite('GET', function() {
+      test('Test GET /api/threads',  function(done){
+        chai.request(server)
+        .get('/api/threads')
+        .query({})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body[0], 'text');
+          assert.property(res.body[0], 'delete_password');
+          done();
+        });
+      });      
       
     });
     
     suite('DELETE', function() {
-      
+      test('Test DELETE /api/threads',  function(done){
+        chai.request(server)
+        .delete('/api/threads')
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body, 'complete delete successful');
+          done();
+        });
+      });    
     });
-    
-    suite('PUT', function() {
-      
-    });
-    
 
   });
-  
-  suite('API ROUTING FOR /api/replies/:board', function() {
-    
-    suite('POST', function() {
-      
-    });
-    
-    suite('GET', function() {
-      
-    });
-    
-    suite('PUT', function() {
-      
-    });
-    
-    suite('DELETE', function() {
-      
-    });
-    
-  });
+
 
 });
